d259fd11b817bcf35522421718b0a64ba8eed965
Your Name <you@example.com>
2022-11-03 17:11:06 -0500

    comit

---
+++ src/main/java/com/tecsup/labo10/junit/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.labo10.junit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
