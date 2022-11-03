72b5d898ba7e1d5ba0131c12806dbf0214f21046
Your Name <you@example.com>
2022-11-03 17:14:41 -0500

    comit2

--- src/main/java/com/tecsup/labo10/junit/App.java
+++ src/main/java/com/tecsup/labo10/junit/App.java
@@ -4,10 +4,23 @@ package com.tecsup.labo10.junit;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		App app = new App();	
+		int sum = app.sumar(4,5);		
+		int res = app.restar(4,5);
+	}
+
+	public  int restar(int i, int j) {
+		// TODO Auto-generated method stub
+		return i-j;
+	}
+
+	public  int sumar(int i, int j) {
+		// TODO Auto-generated method stub
+		return i+j;
+	}
 }
--- src/test/java/com/tecsup/labo10/junit/AppTest.java
+++ src/test/java/com/tecsup/labo10/junit/AppTest.java
@@ -1,20 +1,21 @@
 package com.tecsup.labo10.junit;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+
+	@Test
+	public void testSumar() {		
+		App app = new App();		
+		assertEquals(7 , app.sumar(4, 3));
+	}
+	
+	@Test
+	public void testRestar() {
+		App app = new App();		
+		assertEquals(1 , app.restar(4, 3));		
+	}
+
 }
